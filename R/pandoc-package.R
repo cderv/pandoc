bbd711b38fcb7921364827b550657476789204c5
christophe dervieux <christophe.dervieux@gmail.com>
2021-07-15 15:39:36 +0200

    Fogot to commit this - required to work

---
+++ R/pandoc-package.R
@@ -0,0 +1,11 @@
+#' @keywords internal
+#' @import fs
+#' @import rlang
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
+
+# internal environment acting as storage and cache
+pandocenv <- rlang::new_environment()
