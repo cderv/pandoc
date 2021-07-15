0efad93abc693d80d2d8ee9438251339a267058b
christophe dervieux <christophe.dervieux@gmail.com>
2021-07-15 17:26:15 +0200

    Add tests for MacOS

--- tests/testthat/test-pandoc-install.R
+++ tests/testthat/test-pandoc-install.R
@@ -42,3 +42,15 @@ test_that("Assets are correctly found on linux arm64", {
     expect_match(asset_url, "https://github.com/jgm/pandoc/releases/download", fixed = TRUE)
   })
 })
+
+test_that("Assets are correctly found on linux arm64", {
+  skip_on_cran()
+  skip_if_offline()
+  assets <- .get_assets_info("macOS", NULL)
+  pandoc_2.2.3 <- keep(assets, ~ .x$version == "2.2.3")[[1]]
+  expect_match(pandoc_2.2.3$error, "regression")
+  walk(discard(assets, ~ .x$version == "2.2.3"), ~ {
+    asset_url <- .x[["url"]]
+    expect_match(asset_url, "https://github.com/jgm/pandoc/releases/download", fixed = TRUE)
+  })
+})
