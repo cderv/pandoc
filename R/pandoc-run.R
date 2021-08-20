f3c4214f99c69b0ba883b9ee6cfd7372cb7d4c16
christophe dervieux <christophe.dervieux@gmail.com>
2021-08-20 15:13:11 +0200

    Add pandoc_citeproc_bin() and tests

--- R/pandoc-run.R
+++ R/pandoc-run.R
@@ -5,12 +5,24 @@
 #' * The nightly version e.g `"nightly"`
 #' * The latest installed version `"latest"`
 #'
-#' @return Absolute path to the pandoc binary of the requested version
+#' @return Absolute path to the pandoc binary of the requested version.
 #' @export
 pandoc_bin <- function(version = "default") {
   pandoc_path <- pandoc_locate(version)
   fs::path(pandoc_path, "pandoc",
            ext = ifelse(pandoc_os() == "windows", "exe", ""))
+}
+
+#' @rdname pandoc_bin
+#' @return For `pandoc_citeproc_bin()`, it returns the path to `pandoc-citeproc` binary
+#'   if it exists. Since Pandoc 2.11, the citeproc filter has been included into
+#'   Pandoc itself and is no more shipped as a binary filter.
+pandoc_citeproc_bin <- function(version = "default") {
+  pandoc_path <- pandoc_locate(version)
+  path <- fs::path(pandoc_path, "pandoc-citeproc",
+           ext = ifelse(pandoc_os() == "windows", "exe", ""))
+  if (!fs::file_exists(path)) return(NULL)
+  path
 }
 
 #' Run the pandoc binary from R
--- tests/testthat/test-pandoc-run.R
+++ tests/testthat/test-pandoc-run.R
@@ -1,8 +1,31 @@
-test_that("pandoc_get_version", {
+test_that("pandoc_bin()", {
+  skip_on_cran()
+  skip_if_offline()
   suppressMessages(pandoc_install("2.11.4"))
-  with_pandoc_version("2.11.4", {
-    expect_equal(pandoc_version(), numeric_version("2.11.4"))
-  })
+  suppressMessages(pandoc_install("2.7.3"))
+  local_pandoc_version("2.7.3")
+  expect_match(pandoc_bin(), "2.7.3", fixed = TRUE)
+  expect_match(pandoc_bin("2.11.4"), "2.11.4", fixed = TRUE)
+})
+
+test_that("pandoc_citeproc_bin()", {
+  skip_on_cran()
+  skip_if_offline()
+  suppressMessages(pandoc_install("2.2.1"))
+  suppressMessages(pandoc_install("2.7.3"))
+  suppressMessages(pandoc_install("2.11.4"))
+  local_pandoc_version("2.7.3")
+  expect_match(pandoc_citeproc_bin(), "2.7.3", fixed = TRUE)
+  expect_match(pandoc_citeproc_bin("2.2.1"), "2.2.1", fixed = TRUE)
+  expect_null(pandoc_citeproc_bin("2.11.4"))
+})
+
+test_that("pandoc_get_version()", {
+  skip_on_cran()
+  skip_if_offline()
+  suppressMessages(pandoc_install("2.11.4"))
+  local_pandoc_version("2.11.4")
+  expect_equal(pandoc_version(), numeric_version("2.11.4"))
 })
 
 
