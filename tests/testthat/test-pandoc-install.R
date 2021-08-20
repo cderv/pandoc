abf0d4b10368405e80dc78fda93aa9b42ba887cf
christophe dervieux <christophe.dervieux@gmail.com>
2021-08-20 15:21:25 +0200

    suppress unwanting warning

--- tests/testthat/test-pandoc-install.R
+++ tests/testthat/test-pandoc-install.R
@@ -13,8 +13,8 @@
 }
 
 expect_pandoc_installed <- function(version) {
-  install_dir <- pandoc_locate(version)
-  if (pandoc_is_installed(version)) fs::dir_delete(install_dir)
+  install_dir <- suppressWarnings(pandoc_locate(version))
+  if (!rlang::is_null(install_dir)) fs::dir_delete(install_dir)
   install_dir <- suppressMessages(pandoc_install(version))
   bin <- fs::path(install_dir, "pandoc",
                   ext = ifelse(pandoc_os() == "windows", "exe", ""))
