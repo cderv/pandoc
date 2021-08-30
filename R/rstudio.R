669c0738b30051eb86be3644c6cda091dee0aa1e
christophe dervieux <christophe.dervieux@gmail.com>
2021-08-30 14:17:48 +0200

    Rename functions for rstudio's pandoc

--- R/rstudio.R
+++ R/rstudio.R
@@ -5,14 +5,14 @@
 #'
 #' @export
 #' @name rstudio_pandoc
-pandoc_version_rstudio <- function() {
-  path <- pandoc_bin_rstudio()
+pandoc_rstudio_version <- function() {
+  path <- pandoc_rstudio_bin()
   if (is.null(path)) return(NULL)
   pandoc_version(bin = path)
 }
 
 #' @rdname rstudio_pandoc
-pandoc_bin_rstudio <- function() {
+pandoc_rstudio_bin <- function() {
   pandoc_rstudio_bin <- Sys.getenv("RSTUDIO_PANDOC")
   if (is.na(pandoc_rstudio_bin)) return(NULL)
   pandoc_bin_impl(pandoc_rstudio_bin)
