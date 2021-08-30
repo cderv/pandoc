ac9205514bb363d817e8ec4c28ff76775b072c6b
christophe dervieux <christophe.dervieux@gmail.com>
2021-08-30 14:16:24 +0200

    Add a function for the path to and export those

--- R/rstudio.R
+++ R/rstudio.R
@@ -1,8 +1,19 @@
-#' Retrieve version of Pandoc shipped with RStudio
+#' Retrieve path and version of Pandoc shipped with RStudio
 #'
-#' @noRd
+#' RStudio IDE ships with a pandoc binary. The PATH is stored in `RSTUDIO_PANDOC`
+#' environment variable. Theses function are helper to easily use this specific version.
+#'
+#' @export
+#' @name rstudio_pandoc
 pandoc_version_rstudio <- function() {
+  path <- pandoc_bin_rstudio()
+  if (is.null(path)) return(NULL)
+  pandoc_version(bin = path)
+}
+
+#' @rdname rstudio_pandoc
+pandoc_bin_rstudio <- function() {
   pandoc_rstudio_bin <- Sys.getenv("RSTUDIO_PANDOC")
   if (is.na(pandoc_rstudio_bin)) return(NULL)
-  pandoc_version(bin = pandoc_bin_impl(pandoc_rstudio_bin))
+  pandoc_bin_impl(pandoc_rstudio_bin)
 }
