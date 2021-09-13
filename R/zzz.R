73ae35dae90a277e547220f242711b60a7d1e4a4
christophe dervieux <christophe.dervieux@gmail.com>
2021-09-13 15:55:13 +0200

    Set active_version to the first one found of installed > rstudio > system

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,17 @@
 .onLoad <- function(libname, pkgname) {
-  latest <- pandoc_installed_latest()
-  if (!is.null(latest)) the$active_version <- latest
+  # Set the active version to the first Pandoc binary found between:
+  # * "latest" Pandoc version installed with this package
+  # * "rstudio" version. Will be always found in the RStudio IDE
+  # * "system" version. When one version is available on PATH
+  latest_bin <- pandoc_installed_latest()
+  rstudio_bin <- pandoc_which_bin("rstudio")
+  system_bin <- pandoc_which_bin("system")
+  if (!is.null(latest_bin)) {
+    the$active_version <- latest_bin
+  } else if (!is.null(rstudio_bin)) {
+    the$active_version <- "rstudio"
+  } else if (!is.null(system_bin)) {
+    the$active_version <- "system"
+  }
 }
 
