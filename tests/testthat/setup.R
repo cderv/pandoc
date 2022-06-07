9f1457236bed631284e0aa9e03937955c8144984
Christophe Dervieux <christophe.dervieux@gmail.com>
2022-06-07 13:27:59 +0200

    Deactivate rmarkdown activation by default

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -2,6 +2,12 @@
 if (!isTRUE(as.logical(Sys.getenv("CI")))) devmode(TRUE)
 withr::defer(devmode(FALSE), teardown_env())
 
+# Don't activate rmarkdown by default when using activation function
+old_opts <- options(pandoc.activate_rmarkdown = FALSE)
+withr::defer(options(old_opts), teardown_env())
+
 # run first
 message(">> pandoc_home(): ", pandoc_home())
 message(">> Cached found: ", fs::file_exists(Sys.getenv("PANDOC_CACHE_GITHUB", NA_character_)))
+message(">> R Markdown pandoc: ", paste0(the$rmarkdown_active_version$dir, "-", the$rmarkdown_active_version$version))
+message(">> Active version: ", the$active_version)
