0adb17731086202946648b268142c454e6901da7
Christophe Dervieux <christophe.dervieux@gmail.com>
2022-09-26 20:45:32 +0200

    skip earlier when gh not installed

--- tests/testthat/test-pandoc-browse.R
+++ tests/testthat/test-pandoc-browse.R
@@ -3,11 +3,11 @@ test_that("url_view", {
 })
 
 test_that("pandoc_browse_release()", {
+  skip_if_not_installed("gh")
   # trick to cache releases if not yet to avoid message
   suppressMessages(pandoc_releases())
   expect_snapshot(pandoc_browse_release())
   expect_snapshot(pandoc_browse_release("2.7.3"))
-  skip_if_not_installed("gh")
   expect_error(pandoc_browse_release("0.1"))
 })
 
