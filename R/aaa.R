45960a34000526a043178cea0482ea6689fa7b5c
Christophe Dervieux <christophe.dervieux@gmail.com>
2025-07-01 18:11:35 +0200

    Add a comment based on quarto-r package experience

--- R/aaa.R
+++ R/aaa.R
@@ -7,10 +7,12 @@ the <- rlang::new_environment(
     # Possible pandoc installation not managed by this package directly
     external_versions = c("system", "rstudio"),
     # rmarkdown pandoc version
-    rmarkdown_active_version = if (rlang::is_installed("rmarkdown")) tryCatch(rmarkdown::find_pandoc(), error = function(e) NULL),
+    rmarkdown_active_version = if (rlang::is_installed("rmarkdown")) {
+      tryCatch(rmarkdown::find_pandoc(), error = function(e) NULL)
+    },
     rmarkdown_old_active_version = NULL
 
-    # other possibles values in this env
+    # other possibles values in this env but should not exist because of env_cache usage
 
     # Information about available pandoc releases fetch on Github API
     # pandoc_releases =
